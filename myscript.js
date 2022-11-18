9c0710242f4af3afb1074bc60b9f5a18a6cdd88d
Enzo-belardo <vincenzobelardo10@gmail.com>
2022-11-18 16:23:48 +0100

    ultimo push

--- myscript.js
+++ myscript.js
@@ -2,7 +2,7 @@ let nome = prompt('inserisci nome');
 let cognome= prompt('inserisci cognome');
 let colore= prompt('inserisci colore preferito');
 
-let pass=  nome + cognome + colore
-console.log('pass');
+let pass=  nome + cognome + colore + '22'
+console.log(pass);
 
-document.getElementById('output').innerHTML= "nome" + "cognome" + "colore" ;
+document.getElementById('output').innerHTML= pass;
